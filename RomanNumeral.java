bad291069ab44356b0c435a6054e51aabdf14b09
Kandulap2510 <praneethkandula05@gmail.com>
2018-02-27 10:24:32 -0800

    Update RomanNumeral.java

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -1,4 +1,10 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -21,20 +27,22 @@ public class RomanNumeral
 	{
 
 
+		setRoman(str);
 
 	}
 
 	public RomanNumeral(Integer orig)
 	{
 
+		setNumber(orig);
 
 
 	}
 
 	public void setNumber(Integer num)
-	{
-
-
+	{		
+		number = num;
+		
 
 
 
@@ -43,17 +51,82 @@ public class RomanNumeral
 	public void setRoman(String rom)
 	{
 
+		roman = rom;
 
 
 	}
 
 	public Integer getNumber()
 	{
-		return number;
+		int value = 0;
+		if(roman.length() == 1) {
+			for(int i = 0; i < LETTERS.length; i++){
+				if (roman.equals(LETTERS[i])) {
+					value = NUMBERS[i];
+				}
+			}
+		} 
+		
+		else {
+			while (roman.length() > 0){
+				for(int i = 0; i<LETTERS.length; i++){
+					if(roman.length() > 1) {
+						if (roman.substring(0, 2).equals(LETTERS[i])){
+							roman = roman.substring(2, roman.length());
+							
+							value = value + NUMBERS[i];
+							
+							i = -1;
+
+
+						}
+						else if (roman.substring(0, 1).equals(LETTERS[i])){
+							roman = roman.substring(1,roman.length());
+							
+							value = value + NUMBERS[i];
+							
+							i = -1;
+
+						}
+					}
+					else if(roman.length() == 1) {
+						for(int k = 0; k<LETTERS.length; k++){
+							if (roman.equals(LETTERS[k])) {
+								roman = " ";
+								
+								value = value + NUMBERS[k];
+							}
+						}
+					}
+				}
+			}
+		}	
+		return value;
+	}
+	
+	public String getRoman() {
+		String roman2 = "";
+		
+		
+		while (number != 0) { 
+			for (int i = 0; i < NUMBERS.length; i++)
+			{
+				
+				if (number >= NUMBERS[i]) {
+					
+					number = number - NUMBERS[i];
+					roman2 = roman2 + LETTERS[i];
+					
+					i = -1;
+
+				}
+			}
+		}
+		return roman2;
 	}
 
 	public String toString()
 	{
-		return roman + "\n";
+		return getRoman() + "\n";
 	}
 }
