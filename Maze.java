c8714d1f9815b3f1e3375d9f72e039a25198cfb1
Kandulap2510 <praneethkandula05@gmail.com>
2018-03-26 11:05:03 -0700

    Added to folder

--- Maze.java
+++ Maze.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -12,31 +12,70 @@ import static java.lang.System.*;
 public class Maze
 {
    private int[][] maze;
+   boolean exitFound;
 
 	public Maze()
 	{
-
+		maze = new int[0][0];
 
 	}
 
 	public Maze(int size, String line)
 	{
+		exitFound = false;
+		Scanner file = new Scanner(line);
+		maze = new int[size][size];
+		for (int i = 0; i < maze.length; i++){
+			for (int j = 0; j < maze[i].length; j++)
+			{
+				maze [i][j] = file.nextInt();
+				
+			}
+		}
 
 
 	}
 
 	public boolean hasExitPath(int r, int c)
 	{
-
-		return false;
+		if ((r >= 0 && r < maze.length) && (c >= 0 && c < maze[r].length) && (maze[r][c] == 1)){
+			maze [r][c] = 0;
+			if (c == maze[r].length -1)
+			{
+				exitFound = true;
+			}
+			else
+			{
+				hasExitPath(r+1, c);
+				hasExitPath(r-1, c);
+				hasExitPath(r, c+1);
+				hasExitPath(r, c -1);
+				
+			}
+		}
+		return exitFound;
 	}
 
 	public String toString()
 	{
+		
+
 		String output="";
-
-
-
+		for (int i = 0; i < maze.length; i++){
+			for (int j = 0; j < maze.length; j++)
+			{
+				output = output + maze[i][j] + " ";
+			}
+			output = output + "\n";
+		}
+		if (hasExitPath(0,0))
+		{
+			output = output + "Exit is found";
+		}
+		else
+		{
+			output = output + "Exit is not found";
+		}
 
 
 		return output;
