462a66785f28a0d2aac3ce0d813735d9dc46a938
Kandulap2510 <praneethkandula05@gmail.com>
2018-03-30 09:36:43 -0700

    Unit 13 finished - code changed

--- NumberSort.java
+++ NumberSort.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -12,62 +12,37 @@ import static java.lang.System.*;
 
 public class NumberSort
 {
-	//instance variables and other methods not shown
+	
 
 	private static int getNumDigits(int number)
 	{
 		int count = 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
+		while (number > 0)
+		{
+			number = number/10;
+			count++;
+			
+		}
 		return count;
 	}
 	
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		int[] sorted = new int[getNumDigits(number)];
+		for (int i =0; i < sorted.length; i++)
+		{
+			sorted[i] = number %10;
+			number = number /10;
+		}
+		for (int i=0; i< sorted.length; i++){
+			for (int j=0; j< sorted.length-1; j++){
+				if (sorted[j] > sorted[j+1]){
+					int temp = sorted[j];
+					sorted[j] = sorted[j+1];
+					sorted[j+1] = temp;
+				}
+			}
+		}
 		return sorted;
 	}
 }
